f136a0b54f6b3d23c273d21432b851d4e230dc01
elbazuk <elbazuk@users.noreply.github.com>
2017-01-21 18:26:03 +0000

    Update index.php

--- view/default/template/views/index.php
+++ view/default/template/views/index.php
@@ -1,7 +1,13 @@
-<h1>Welcome to Lectric</h1>
-
-<h4>Congrats, dude! If you're seeing this, it means the installation was <em>almost</em> a success.</h4>
-
-<p>So we already know you can VIEW things, but can you DO things? To find out - <span class="test_do">click here</span>.</p> 
-
+<h1>Welcome to Lectric</h1>
+
+
+
+<h4>Congrats, dude! If you're seeing this, it means the installation was <em>almost</em> a success.</h4> 
+
+
+
+<p>So we already know you can VIEW things, but can you DO things? To find out - <span class="test_do">click here</span>.</p> 
+
+
+
 <div class="response"></div>
