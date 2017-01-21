a8dc3ee7adccf58b6b6bd548279e7288e0766ca4
elbazuk <elbazuk@users.noreply.github.com>
2017-01-21 18:24:17 +0000

    Update test.php

--- do/lectric/test.php
+++ do/lectric/test.php
@@ -1,11 +1,21 @@
-<?php
-
-	if (isset($_POST['do'])){
-		
-		?><p>Congrats, this message means you can DO things too! Here's some info about your current configuration:</p><?php
-		?><p class="end">The SESSION is <?php echo (isset($_SESSION)) ? 'ON':'OFF'; ?>: /engine/plugin/core_coinfig.php -> SESSION_IGNORES definition.</p><?php
-		?><p class="end">The default view directory is <?php echo DEFAULT_DIRECTORY; ?>: /engine/plugin/core_coinfig.php -> DEFAULT_DIRECTORY definition.</p><?php
-		?><p class="end">The Project Root is <?php echo DOC_ROOT; ?></p><?php
-		?><p class="end">The Request URL for this do action is <?php echo URL_REQUEST; ?></p><?php
-		
-	}
+<?php
+
+
+
+	if (isset($_POST['do'])){
+
+		
+
+		?><p>Congrats, this message means you can DO things too! Here's some info about your current configuration:</p><?php
+
+		?><p class="end">The SESSION is <?php echo (isset($_SESSION)) ? 'ON':'OFF'; ?>: /engine/plugin/core_coinfig.php -> SESSION_IGNORES definition.</p><?php
+
+		?><p class="end">The default view directory is <?php echo DEFAULT_DIRECTORY; ?>: /engine/plugin/core_coinfig.php -> DEFAULT_DIRECTORY definition.</p><?php
+
+		?><p class="end">The Project Root is <?php echo DOC_ROOT; ?></p><?php
+
+		?><p class="end">The Request URL for this do action is <?php echo URL_REQUEST; ?></p><?php
+
+		
+
+	} 
