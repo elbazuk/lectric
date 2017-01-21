910c7f75e72860bccf906b5dd8debf3b15cb07d0
elbazuk <elbazuk@users.noreply.github.com>
2017-01-21 18:25:45 +0000

    Update footer.php

--- view/default/template/common/footer.php
+++ view/default/template/common/footer.php
@@ -1,12 +1,23 @@
-
-	<footer class="clear">
-	</footer>
-	
-	<script src="/view/default/js/script.js"></script>
-	
-	<?php  include (DOC_ROOT.'/view/default/js/'.$this->page['webpages']['js']); ?>
-     
- </section><!--body wrapper-->
-
-</body>
+
+
+	<footer class="clear">
+
+	</footer>
+
+	
+
+	<script src="/view/default/js/script.js"></script> 
+
+	
+
+	<?php  include (DOC_ROOT.'/view/default/js/'.$this->page['webpages']['js']); ?>
+
+     
+
+ </section><!--body wrapper-->
+
+
+
+</body>
+
 </html>
