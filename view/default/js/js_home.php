d02ee71d2360d66e2595738b26a0adb6bfcdcdf4
elbazuk <elbazuk@users.noreply.github.com>
2017-01-21 18:26:11 +0000

    Update js_home.php

--- view/default/js/js_home.php
+++ view/default/js/js_home.php
@@ -1,20 +1,39 @@
-<script>
-
-	$('.test_do').on('click', function(){
-		
-		$.ajax({
-			cache: false,
-			type: 'POST',
-			url: '/do/lectric/test',
-			data: 'do=it',
-			success: function (response){
-				$('.response').html(response);
-			},
-			error: function(){
-				alert('Failed to reach do script');
-			}
-		});
-		
-	});
-
+<script>
+
+
+
+	$('.test_do').on('click', function(){ 
+
+		
+
+		$.ajax({
+
+			cache: false,
+
+			type: 'POST',
+
+			url: '/do/lectric/test',
+
+			data: 'do=it',
+
+			success: function (response){
+
+				$('.response').html(response);
+
+			},
+
+			error: function(){
+
+				alert('Failed to reach do script');
+
+			}
+
+		});
+
+		
+
+	});
+
+
+
 </script>
