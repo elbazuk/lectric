19143b1ac91ebf564db0da20f0f3ca52250ef33d
elbazuk <elbazuk@users.noreply.github.com>
2017-01-21 18:26:38 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<?php
-
-/**
-* call up the bootstrap initialiser
-*/
-
-if (file_exists(__DIR__.'/engine/init.php')) {
-	require(__DIR__.'/engine/init.php');
-} else {
-	echo 'init file not found';
-	exit;
+<?php
+
+
+
+/**
+
+* call up the bootstrap initialiser 
+
+*/
+
+
+
+if (file_exists(__DIR__.'/engine/init.php')) {
+
+	require(__DIR__.'/engine/init.php');
+
+} else {
+
+	echo 'init file not found';
+
+	exit;
+
 }
