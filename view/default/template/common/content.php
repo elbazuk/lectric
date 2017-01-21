4e0837cccfb958e250f00cd39058960dc1d9313b
elbazuk <elbazuk@users.noreply.github.com>
2017-01-21 18:25:39 +0000

    Update content.php

--- view/default/template/common/content.php
+++ view/default/template/common/content.php
@@ -1,8 +1,15 @@
-<?php
-
-	/*
-	* Include the webpage file if exists
-	*/
-		if (file_exists(DOC_ROOT.'/view/default/template/views/'.$this->page['webpages']['url'].'.php')) {
-			include(DOC_ROOT.'/view/default/template/views/'.$this->page['webpages']['url'].'.php');
+<?php
+
+
+
+	/*
+
+	* Include the webpage file if exists 
+
+	*/
+
+		if (file_exists(DOC_ROOT.'/view/default/template/views/'.$this->page['webpages']['url'].'.php')) {
+
+			include(DOC_ROOT.'/view/default/template/views/'.$this->page['webpages']['url'].'.php');
+
 		} 
