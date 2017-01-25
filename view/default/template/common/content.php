4f3f14b2a413396a47d56425af58f48337718184
elbazuk <elliott.baratt@gmail.com>
2017-01-25 12:58:25 +0000

    performance and security upgrades

--- view/default/template/common/content.php
+++ view/default/template/common/content.php
@@ -1,8 +1,8 @@
-<?php
-
-	/*
-	* Include the webpage file if exists 
-	*/
-		if (file_exists(DOC_ROOT.'/view/'.$this->_fileDirectory.'/template/views/'.$this->page['url'].'.php')) {
-			include(DOC_ROOT.'/view/'.$this->_fileDirectory.'/template/views/'.$this->page['url'].'.php');
-		} 
+<?php
+
+	/*
+	* Include the webpage file if exists 
+	*/
+		if (file_exists(DOC_ROOT.'/view/'.$this->_fileDirectory.'/template/views/'.$this->page['url'].'.php')) {
+			include(DOC_ROOT.'/view/'.$this->_fileDirectory.'/template/views/'.$this->page['url'].'.php');
+		} 
