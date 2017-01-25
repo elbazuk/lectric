1271ee1e381050fed508c0d87d9487cd016ab28c
Elliott Barratt <erbarratt@users.noreply.github.com>
2017-01-25 13:00:15 +0000

    Update config.php

--- engine/config/config.php
+++ engine/config/config.php
@@ -5,17 +5,8 @@
 	*/
 		mb_internal_encoding('UTF-8');	// Tell PHP that we're using UTF-8 strings until the end of the script use mb_ for string functions...
 		mb_http_output('UTF-8'); 		// Tell PHP that we'll be outputting UTF-8 to the browser 
-		
 
-	/* 
-	* Check version 
-	*/
-		if (phpversion() != '7.1.0'){
-			echo 'This framework only supports PHP >= 7.1.* ';
-			exit;
-		}
-		
-	
+
 	/*
 	* naive performance metrics
 	*/
@@ -27,8 +18,17 @@
 	* naive performance function
 	*/
 		function lec_rutime($ru, $rus, $index) {
-			return ($ru["ru_$index.tv_sec"]*1000 + intval($ru["ru_$index.tv_usec"]/1000))
-			 -  ($rus["ru_$index.tv_sec"]*1000 + intval($rus["ru_$index.tv_usec"]/1000));
+			return ($ru['ru_'.$index.'.tv_sec']*1000 + intval($ru['ru_'.$index.'.tv_usec']/1000))
+			 -  ($rus['ru_'.$index.'.tv_sec']*1000 + intval($rus['ru_'.$index.'.tv_usec']/1000));
+		}
+		
+		
+	/* 
+	* Check version 
+	*/
+		if (phpversion() != '7.1.0'){
+			echo 'This framework only supports PHP >= 7.1.* ';
+			exit;
 		}
 		
 	
@@ -43,23 +43,17 @@
 	*/
 		if (file_exists(DOC_ROOT.'/engine/plugin/core_config.php')) {
 			require(DOC_ROOT.'/engine/plugin/core_config.php');
-		} else {
-			echo 'core config file not found';
-			exit;
 		}
 		
 		
 	/*
 	* set up display warnings for debug, defaults to true
+	* default to error reporting on, unless core_config debug definition overrides in /engine/plugin/core_config
 	*/
 		if (!defined('DEBUG')){
 			define ('DEBUG', true);
 		}
 		
-		
-	/*
-	* default to error reporting on, unless core_config debug definition overrides in /engine/plugin/core_config
-	*/	
 		if (DEBUG){
 			error_reporting(E_ALL);
 			ini_set('display_errors', '1');
@@ -69,62 +63,35 @@
 	/*
 	* Default constants - override these in /engine/plugin/core_config
 	*/
-	
 		if (!defined('SITE_NAME')){ define('SITE_NAME','Lectric'); }									//for ,eta title
 		if (!defined('SITE_LINK')){ define('SITE_LINK',$_SERVER['SERVER_NAME']); } 						//url, defaults to nothing
 		if (!defined('SITE_DESCRIPTION')){ define('SITE_DESCRIPTION','Lectric Default Installation'); }	//for meta desc
 		if (!defined('DEFAULT_DIRECTORY')){ define('DEFAULT_DIRECTORY','default'); }					//for view directory selection
 		if (!defined('SESSION_IGNORES')){ define('SESSION_IGNORES', []); }								//if scripts need to set own headers, they can be ignored for seesion start further down.
 		
+		
 	/*
-	* define base URL NODES for controller
+	* define base URL NODES, URL_REQUEST AND REQUEST_METHOD
 	*/
-		if (isset($_SERVER['REQUEST_URI'])){
-			define ('URL_REQUEST', $_SERVER['REQUEST_URI']);
-		} else {
-			define ('URL_REQUEST', '/');
-		}
+		//base request
+		define ('URL_REQUEST', $_SERVER['REQUEST_URI']);
+		define('URL_PATH', parse_url(URL_REQUEST, PHP_URL_PATH));
+		define ('REQUEST_METHOD', $_SERVER['REQUEST_METHOD']);
+		
+		//re-register egt
+		define('REQUEST_QUERY_STRING', parse_url(URL_REQUEST, PHP_URL_QUERY));
+		mb_parse_str(REQUEST_QUERY_STRING, $_GET);
 
-		$lecNodes = explode('/', trim(URL_REQUEST, '/'));
+		//set up nodes
+		$lecNodes = explode('/', trim(URL_PATH, '/')); //trim important for URL_NODES index numbers
 		$lecNodes = array_filter( $lecNodes, function($value) { return $value !== ''; });
-		//register get params 
-		$nodeCount = count($lecNodes);
-		$endNode = end($lecNodes);
-		$bits = explode('?', $endNode);
-		$bits = array_filter( $bits, function($value) { return $value !== ''; });
-		if(count($bits)>1){
-			
-			$morebits = explode('&', $bits[1]);
-			if (count($morebits) > 1){
-				foreach($morebits as $value){
-					$finalbits = explode('=', $value);
-					if (isset($finalbits[1])){
-						$_GET[$finalbits[0]] = $finalbits[1];
-					}
-				}
-			} else {
-				$finalbits = explode('=', $morebits[0]);
-				if (isset($finalbits[1])){
-					$_GET[$finalbits[0]] = $finalbits[1];
-				}
-			}
-			
-			$lecNodes[$nodeCount-1] = $bits[0];
-			
-		}
-		
 		define('URL_NODES', $lecNodes);
-		unset($lecNodes);
 		
 		
 	/*
-	* define if view or not
+	* define if view or not 
 	*/
-		if (!isset(URL_NODES[0])){
-			define('VIEW', true);										//home "/"
-		} else {
-			define('VIEW', (URL_NODES[0] !== 'do') ? true : false);		//do or view?
-		}
+		define('VIEW', (URL_PATH === '/') ? true : ((URL_NODES[0] !== 'do') ? true : false) );		//do or view?
 		
 	
 	/*
@@ -161,6 +128,7 @@
 		} else {
 			$lecDBH = null;
 		}
+		
 		
 	/* 
 	* SESSION 
