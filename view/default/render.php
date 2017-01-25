0fdd1967044040812929f65f9cf992a697088a38
elbazuk <elliott.baratt@gmail.com>
2017-01-25 13:01:42 +0000

    upgrades

--- view/default/render.php
+++ view/default/render.php
@@ -1,20 +1,20 @@
-<?php
-
-	/*
-	* load up webpage based on PAGE_URL 
-	*/
-		$this->page = $this->loadPage();
-		
-	/*
-	* Limit view to only index <-REMOVE THIS CALL IF COPYING TO DEVELOP WITH
-	*/
-		if ($this->_pageUrl !== 'index' || $this->_URLdirectory !== 'root'){
-			(new \Lectric\controlAction('view', '/'))->performAction();
-		}
-
-	/*
-	* Include each template part from directory. Add switch here to facilitate other directories.
-	*/
-		include(DOC_ROOT.'/view/'.$this->_fileDirectory.'/template/common/header.php');
-		include(DOC_ROOT.'/view/'.$this->_fileDirectory.'/template/common/content.php');
-		include(DOC_ROOT.'/view/'.$this->_fileDirectory.'/template/common/footer.php'); 
+<?php
+
+	/*
+	* load up webpage based on PAGE_URL 
+	*/
+		$this->page = $this->loadPage();
+		
+	/*
+	* Limit view to only index <-REMOVE THIS CALL IF COPYING TO DEVELOP WITH
+	*/
+		if ($this->_pageUrl !== 'index' || $this->_URLdirectory !== 'root'){
+			(new \Lectric\controlAction('view', '/'))->performAction();
+		}
+
+	/*
+	* Include each template part from directory. Add switch here to facilitate other directories.
+	*/
+		include(DOC_ROOT.'/view/'.$this->_fileDirectory.'/template/common/header.php');
+		include(DOC_ROOT.'/view/'.$this->_fileDirectory.'/template/common/content.php');
+		include(DOC_ROOT.'/view/'.$this->_fileDirectory.'/template/common/footer.php'); 
--- view/default/template/views/index.php
+++ view/default/template/views/index.php
@@ -1,21 +1,21 @@
-
-	<?php 
-		if (($lecMessages = \Lectric\controller::getSessionMessages()) !== null){
-			
-			?><div class="unit-25 unit-centered"><div class="tools-alert tools-alert-yellow"><?php
-				foreach ($lecMessages as $msg){
-					echo $msg.'<br/>';
-				}	
-				\Lectric\controller::clearSessionMessages();
-			?></div></div><?php
-			
-		} else {
-	?>
-
-		<?php echo $this->page['html']; ?>
-
-		<p>So we already know you can VIEW things, but can you DO things? To find out - <span class="test_do">click here</span>.</p> 
-
-		<div class="response"></div>
-
+
+	<?php 
+		if (($lecMessages = \Lectric\controller::getSessionMessages()) !== null){
+			
+			?><div class="unit-25 unit-centered"><div class="tools-alert tools-alert-yellow"><?php
+				foreach ($lecMessages as $msg){
+					echo $msg.'<br/>';
+				}	
+				\Lectric\controller::clearSessionMessages();
+			?></div></div><?php
+			
+		} else {
+	?>
+
+		<?php echo $this->page['html']; ?>
+
+		<p>So we already know you can VIEW things, but can you DO things? To find out - <span class="test_do">click here</span>.</p> 
+
+		<div class="response"></div>
+
 		<?php }
