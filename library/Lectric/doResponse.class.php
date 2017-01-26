1aa3a1cfc0df8bbdaa5188b24db75f056436f4bc
Elliott Barratt <erbarratt@users.noreply.github.com>
2017-01-26 01:54:45 +0000

    Create doResponse.class.php

---
+++ library/Lectric/doResponse.class.php
@@ -0,0 +1,39 @@
+<?php
+namespace Lectric;
+
+/**
+* do response class
+*
+* @package    Lectric Framework
+* @author     Elliott Barratt
+* @copyright  Elliott Barratt, all rights reserved.
+*
+*/ 
+class doResponse extends SQLQueryPDO
+{
+	/**
+     * The do response construct for calling do response files
+     *
+     * @param array $lecNodes the URL nodes array
+     *
+	 * @param object $DBH db handler 
+	 *
+     */
+		function __construct(array $lecNodes, $DBH)
+		{
+			
+			parent::__construct($DBH);
+			
+			if (count($lecNodes) !== 4){
+				throw new \Exception('wrong do node count for response.');
+			}
+			
+			if (file_exists(DOC_ROOT.'/do/'.$lecNodes[2].'/'.$lecNodes[3].'.php')) {
+				require(DOC_ROOT.'/do/'.$lecNodes[2].'/'.$lecNodes[3].'.php');
+			} else {
+				throw new \Exception('do file not in do directory: '.$lecNodes[2]);
+			}
+			
+		}
+	
+}
