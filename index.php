e6f4965490cc45e778d12170d18b778f355658f7
Elliott Barratt <elliott.baratt@gmail.com>
2018-10-05 00:21:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
 <?php
 
-
-
 /**
-
 * call up the bootstrap initialiser 
-
 */
-
-
-
 if (file_exists(__DIR__.'/engine/init.php')) {
-
 	require(__DIR__.'/engine/init.php');
-
 } else {
-
-	echo 'init file not found';
-
-	exit;
-
+	die('Init file not found');
 }
