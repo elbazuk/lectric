8938771902af57f315faa2cbf56a091e65135eba
Elliott Barratt <elliott.baratt@gmail.com>
2018-10-06 23:56:35 +0100

    Update doResponse.class.php

--- library/Lectric/doResponse.class.php
+++ library/Lectric/doResponse.class.php
@@ -14,24 +14,24 @@ class doResponse extends SQLQueryPDO
 	/**
      * The do response construct for calling do response files
      *
-     * @param array $lecNodes the URL nodes array
+     * @param array URL_NODES the URL nodes array
      *
 	 * @param object $DBH db handler 
 	 *
      */
-		function __construct(array $lecNodes, &$DBH)
+		function __construct(&$DBH)
 		{
 			
 			parent::__construct($DBH);
 			
-			if (count($lecNodes) !== 4){
+			if (count(URL_NODES) !== 4){
 				throw new \Exception('wrong do node count for response.');
 			}
 			
-			if (file_exists(DOC_ROOT.'/do/'.$lecNodes[2].'/'.$lecNodes[3].'.php')) {
-				require(DOC_ROOT.'/do/'.$lecNodes[2].'/'.$lecNodes[3].'.php');
+			if (file_exists(DOC_ROOT.'/do/'.URL_NODES[2].'/'.URL_NODES[3].'.php')) {
+				require(DOC_ROOT.'/do/'.URL_NODES[2].'/'.URL_NODES[3].'.php');
 			} else {
-				throw new \Exception('do file not in do directory: '.$lecNodes[2]);
+				throw new \Exception('do file not in do directory: '.URL_NODES[2]);
 			}
 			
 		}
