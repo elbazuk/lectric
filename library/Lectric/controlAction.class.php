fa8c95537dc9b1b29c88710c9def40ed120c25dd
Elliott Barratt <elliott.baratt@gmail.com>
2018-10-07 17:40:55 +0100

    Update controlAction.class.php

--- library/Lectric/controlAction.class.php
+++ library/Lectric/controlAction.class.php
@@ -17,13 +17,12 @@ class controlAction
 	private $_type = '';
 	private $_location = '';
 	private $_message = '';
-	private $_color = '';
 	
     /**
      *
      * Set member variables for use in performAction()
      */
-		function __construct(string $type = 'none', string $location = '', string $msg = '', string $color = '')
+		function __construct(string $type = 'none', string $location = '', string $msg = '')
 		{
 			
 			switch ($type){
@@ -32,11 +31,9 @@ class controlAction
 					$this->_type = $type;
 					$this->_location = $location;
 					$this->_message = $msg;
-					$this->_color = $color;
 				break;
 				case 'none':
 					$this->_message = $msg;
-					$this->_color = $color;
 				break;
 				default:
 					//do nothing
@@ -59,7 +56,9 @@ class controlAction
 			switch ($this->_type){
 				
 				case 'view':
-					controller::setSessionMessage($this->_message, $this->_color);
+					if (trim($this->_message) !== ''){
+						controller::setSessionMessage($this->_message);
+					}
 					header('Location: '.$this->_location);
 					exit;
 				break;
