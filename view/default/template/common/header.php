bd0ff1b9bdf2950b849138e86386f74a2b82ee8f
Elliott Barratt <elliott.baratt@gmail.com>
2018-10-07 17:41:43 +0100

    Update header.php

--- view/default/template/common/header.php
+++ view/default/template/common/header.php
@@ -1,48 +1,88 @@
-<!doctype html>
-
-<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]--> 
-<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
-<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!-->
-<html lang="en" class="no-js"> <!--<![endif]-->
-
-<head>
-
-    <meta charset="utf-8" />
-	
-    <title>
-		<?php 
-		if ($this->page['metatitle'] != "") {
-				echo $this->page['metatitle'].' | ';
-        }
-		echo SITE_NAME; ?>
-   </title>
-
-	<!--content meta information-->
-    <meta name="author" content="Elliott Barratt" />
-    <meta name="description" content="<?php echo $this->page['metadescription'];?>" />
-    <meta name="keywords" content="<?php echo $this->page['metakeywords'];?>" />
-    
-	<!--favicon-->
-    <link rel="shortcut icon" type="image/png" href="<?php echo $this->_imgLocalDir; ?>/favicon.png" />
-	
-	<!--mobile support meta-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	<meta name="apple-mobile-web-app-capable" content="yes">
-	<meta name="format-detection" content="telephone=no">
-	
-	<!--layout and font stylesheets-->
-	<link rel="stylesheet" href="<?php echo $this->_iconSet; ?>">
-	
-	<!--Main css stylesheet-->
-    <link rel="stylesheet" href="<?php echo $this->_cssLocalDir; ?>/style.css">
-	
-	<script src="<?php echo $this->_jsLibDir; ?>/jquery.js"></script>
-    
-</head>
-
-
-<body >
-
-	<section class="body_wrapper forms">
+<!doctype html>
+
+
+
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]--> 
+
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+
+<!--[if (gt IE 9)|!(IE)]><!-->
+
+<html lang="en" class="no-js"> <!--<![endif]-->
+
+
+
+<head>
+
+
+
+    <meta charset="utf-8" />
+
+	
+
+    <title>
+
+		<?php 
+
+		if ($this->page['metatitle'] != "") {
+
+				echo $this->page['metatitle'].' | ';
+
+        }
+
+		echo SITE_NAME; ?>
+
+   </title>
+
+
+
+	<!--content meta information-->
+
+    <meta name="author" content="Elliott Barratt" />
+
+    <meta name="description" content="<?php echo $this->page['metadescription'];?>" />
+
+    <meta name="keywords" content="<?php echo $this->page['metakeywords'];?>" />
+
+    
+
+	<!--favicon-->
+
+    <link rel="shortcut icon" type="image/png" href="<?php echo $this->_imgLocalDir; ?>/favicon.png" />
+
+	
+
+	<!--mobile support meta-->
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<meta name="apple-mobile-web-app-capable" content="yes">
+
+	<meta name="format-detection" content="telephone=no">	
+
+	<!--Main css stylesheet-->
+
+    <link rel="stylesheet" href="<?php echo $this->_cssLocalDir; ?>/style.css">
+
+	
+
+	<script src="<?php echo $this->_jsLibDir; ?>/jquery.js"></script>
+
+    
+
+</head>
+
+
+
+
+
+<body >
+
+
+
+	<section class="body_wrapper forms">
+
