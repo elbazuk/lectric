da5abf0e34e0b4645c650db1e74e5ab25a942f3e
Elliott Barratt <elliott.baratt@gmail.com>
2018-10-07 17:40:37 +0100

    Update doAction.class.php

--- library/Lectric/doAction.class.php
+++ library/Lectric/doAction.class.php
@@ -15,8 +15,6 @@ class doAction
     /**
      * The do action construct for calling do functions
      *
-     * @param array URL_NODES the URL nodes array
-     *
 	 * @param object $DBH db handler 
 	 *
      */
@@ -24,7 +22,7 @@ class doAction
 		{
 			
 			if (count(URL_NODES) !== 5){
-				throw new \Exception('wrong do node count for action.');
+				throw new \Exception('Wrong Node Count for Do-Action.');
 			}
 			
 			if (class_exists('\\'.URL_NODES[2].'\\'.URL_NODES[3])){
@@ -34,14 +32,15 @@ class doAction
 				
 				if (method_exists($doer, 'do_'.URL_NODES[4])){
 				
+					//do_function returns a controllAction object, which then fires performAction();
 					$action = $doer->{'do_'.URL_NODES[4]}()->performAction();
 				
 				} else {
-					throw new \Exception('Class '.htmlentities('\\'.URL_NODES[2].'\\'.URL_NODES[3]).' doesn\'t contain method '.'do_'.URL_NODES[4].' in do action.');
+					throw new \Exception('Class '.htmlentities('\\'.URL_NODES[2].'\\'.URL_NODES[3].' doesn\'t contain method '.'do_'.URL_NODES[4].' in Do-Action.'));
 				}
 				
 			} else {
-				throw new \Exception('Class '.htmlentities('\\'.URL_NODES[2].'\\'.URL_NODES[3]).' doesn\'t exist for do action.');
+				throw new \Exception('Class '.htmlentities('\\'.URL_NODES[2].'\\'.URL_NODES[3]).' doesn\'t exist for Do-Action.');
 			}
 			
 		}
