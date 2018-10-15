f967c0a92d4d3859cc6d9493ba6284b2a0936441
Elliott Barratt <elliott.baratt@gmail.com>
2018-10-15 19:37:08 +0100

    Update test.php

--- do/lectric/test.php
+++ do/lectric/test.php
@@ -6,12 +6,12 @@
 	if (isset($_POST['do'])){
 		
 		?><p>Congrats, this message means you can DO things too! Here's some info about your current configuration:</p><?php
-		?><p class="end">The SESSION is <?php echo (isset($_SESSION)) ? 'ON':'OFF'; ?>: /engine/plugin/core_coinfig.php -> SESSION_IGNORES definition.</p><?php
-		?><p class="end">The default view directory is <?php echo DEFAULT_DIRECTORY; ?>: /engine/plugin/core_coinfig.php -> DEFAULT_DIRECTORY definition.</p><?php
+		?><p class="end">The SESSION is <?php echo (isset($_SESSION)) ? 'ON':'OFF'; ?>: /engine/app_config.php -> SESSION_IGNORES definition.</p><?php
+		?><p class="end">The default view directory is <?php echo DEFAULT_DIRECTORY; ?>: /engine/app_config.php -> DEFAULT_DIRECTORY definition.</p><?php
 		?><p class="end">The Project Root is <?php echo DOC_ROOT; ?></p><?php
 		?><p class="end">The Request URL for this do action is <?php echo URL_REQUEST; ?></p>
 		<br/>
-		<form action="/do/action/Lectric/lecDefault/test" method="post"><button type="submit" class="btn btn-large btn red">Push Me!</button></form>
+		<form action="/do/action/Lectric/lecDefault/test/" method="post"><button type="submit" class="btn btn-large btn red">Push Me!</button></form>
 		
 		<?php
 		
