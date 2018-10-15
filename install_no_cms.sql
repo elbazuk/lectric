3f988cf616bff0796eae71bac69c5bc6ba9fad68
Elliott Barratt <elliott.baratt@gmail.com>
2018-10-15 18:49:12 +0100

    Create install_no_cms.sql

---
+++ install_no_cms.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS `default_directories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `live` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT INTO `default_directories` (`id`, `name`, `live`) VALUES
+	(1, 'root', 1);
+
+	CREATE TABLE IF NOT EXISTS `default_views` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `pageheading` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `pagesubheading` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `navheading` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `url` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `html` mediumtext COLLATE utf8_bin,
+  `althtml` mediumtext COLLATE utf8_bin,
+  `metatitle` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `metadescription` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `metakeywords` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `directory` tinyint(1) NOT NULL DEFAULT '1',
+  `live` tinyint(1) NOT NULL DEFAULT '0',
+  `sidebar` tinyint(1) NOT NULL DEFAULT '0',
+  `js` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `filename` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `ext` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `permission` varchar(50) COLLATE utf8_bin DEFAULT 'none',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+INSERT INTO `default_views` (`id`, `pageheading`, `pagesubheading`, `navheading`, `url`, `html`, `althtml`, `metatitle`, `metadescription`, `metakeywords`, `directory`, `live`, `sidebar`, `js`, `filename`, `ext`, `permission`) VALUES
+	(1, 'Error', 'Page Not Found', 'Error', 'error', '', NULL, 'Error', '', '', 1, 1, 0, '', '', '', 'none'),
+	(2, 'Lectric', 'Lectric', 'Lectric', 'index', '<h1>Welcome to Lectric</h1>\r\n<h4>Congrats, dude! If you\'re seeing this, it means the installation was <em>almost</em> a success.</h4>', '', 'Lectric Framework', 'Homepage of the Lectric Framework default view directory', '', 1, 1, 0, 'js_home.php', 'favicon.png', '', 'none');
+
+	CREATE TABLE IF NOT EXISTS `lec-admin_directories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `live` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
