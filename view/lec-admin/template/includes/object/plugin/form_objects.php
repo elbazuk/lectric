1c0db73263bdfeead456a0bec4323620ecbd43e8
Elliott Barratt <elliott.baratt@gmail.com>
2018-10-30 14:44:19 +0000

    Update form_objects.php

--- view/lec-admin/template/includes/object/plugin/form_objects.php
+++ view/lec-admin/template/includes/object/plugin/form_objects.php
@@ -1,18 +1,21 @@
-<hr/><br/>
-Example Field Structure<br/>
-<code>
-<?php echo str_replace('	','&nbsp;&nbsp;&nbsp;&nbsp;',nl2br('[
-	{
-		"name":"Field",
-		"field":"field",
-		"placeholder":"something",
-		"form_type":"text",
-		"select_table":"",
-		"select_field":"",
-		"mandatory":"yes",
-		"edit_type":"text",
-		"class_inj":"",
-		"help_text":""
-	}
-]'));?>
+<hr/><br/>
+Example Field Structure<br/>
+<code>
+	
+<?php echo str_replace('	','&nbsp;&nbsp;&nbsp;&nbsp;',nl2br('[
+	{
+		"name":"Field",
+		"field":"field",
+		"placeholder":"something",
+		"form_type":"text",
+		"select_table":"",
+		"select_field":"",
+		"mandatory":"yes",
+		"edit_type":"text",
+		"class_inj":"",
+		"help_text":"",
+		"highlight_in_list":"yes"
+	}
+]'));?>
+	
 </code>
