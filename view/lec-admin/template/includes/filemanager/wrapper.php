b8dc0fa6431bf44d20135ebd6318a9ef5e05b1d0
Elliott Barratt <elliott.baratt@gmail.com>
2018-11-09 01:27:34 +0000

    Create wrapper.php

---
+++ view/lec-admin/template/includes/filemanager/wrapper.php
@@ -0,0 +1,25 @@
+<!--Main css stylesheet-->
+<link rel="stylesheet" href="/view/lec-admin/css/style.css">
+<script src="/view/lec-admin/js/plugins.js"></script>
+<style>
+	<?php $this->getCSS(); ?>
+</style>
+<div class="page">
+<div class="fm_wrapper">
+
+	<!--main record of directory-->
+	<input type="hidden" name="fm_global_dir" id="fm_global_dir" value="<?php echo $directory; ?>" /> 
+
+	<?php include(DOC_ROOT.$this->_filemanager_top_bar_template); ?>
+
+	<?php $this->loadDirectoryContentsHTML($directory); ?>
+	
+	<div class="fm_footer">
+		<div class="fm_message"></div>
+		Allowed File Types: <?php echo implode(', ', $this->_allowed_file_types); ?>
+	</div>
+
+</div><!--fm_wrapper-->
+</div>
+
+<?php $this->getJS(); ?>
