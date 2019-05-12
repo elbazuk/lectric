c2788039b4aef8eb56229cd1f93f123f1dc4e799
Elliott Barratt <elbazuk@gmail.com>
2019-05-12 15:38:56 +0100

    Update form_objects.php

--- view/lec-admin/template/includes/object/plugin/form_objects.php
+++ view/lec-admin/template/includes/object/plugin/form_objects.php
@@ -4,20 +4,20 @@ Example Field Structure<br/>
 	
 <?php echo str_replace('	','&nbsp;&nbsp;&nbsp;&nbsp;',nl2br('[
 	{
-		"name":"Field",			//label of the input
-		"field":"field",		//db table
+		"name":"Field",				//label of the input
+		"field":"field",			//db table
 		"placeholder":"something",	//input placeholder
-		"form_type":"text",		//input type (text, textarea, select, select_yesno, date etc)
-		"select_table":"",		//table for selects
-		"select_field":"",		//which field for select option labels
-		"mandatory":"yes",		//is the field mandatory?
-		"edit_type":"text",		//how is the data stored? (text, html, number etc)
-		"class_inj":"",			//any classes for inputs to be injected into that input?
-		"help_text":"",			//forms-desc text under input
+		"form_type":"text",			//input type (text, textarea, select, select_yesno, date etc)
+		"select_table":"",			//table for selects
+		"select_field":"",			//which field for select option labels
+		"mandatory":"yes",			//is the field mandatory?
+		"edit_type":"text",			//how is the data stored? (text, html, number etc)
+		"class_inj":"",				//any classes for inputs to be injected into that input?
+		"help_text":"",				//forms-desc text under input
 		"highlight_in_list":"yes",	//is a yes/no field, make it highlight?
-		"read_only":"no",		//is input read only?
-		"sortable":"no",		//can you sort on this field in the object list?
-		"half_width":"no"		//is the input half width on the form?
+		"read_only":"no",			//is input read only?
+		"sortable":"no",			//can you sort on this field in the object list?
+		"half_width":"no"			//is the input half width on the form?
 	}
 ]'));?>
 	
