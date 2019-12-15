2594c6f6868aa2e4e2929862381415f0730b25f1
Elliott Barratt <elbazuk@gmail.com>
2019-12-15 00:35:15 +0000

    Update script.js

--- view/lec-admin/js/script.js
+++ view/lec-admin/js/script.js
@@ -1,15 +1,1 @@
 
-//filemanager stuff
-	setFileManager();
-
-	$('body').on('click', '.filemanager_button', function(){
-
-		window.filemanager_field = $(this).attr('data-field');
-		
-		if(window.addEventListener){
-			window.addEventListener('message', filemanager_onMessage, false);
-		} else {
-			window.attachEvent('onmessage', filemanager_onMessage);
-		}
-		
-	});
