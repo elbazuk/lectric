7c69b3ab9734a5e79c349464e3872514d5ef4a35
Elliott Barratt <elbazuk@gmail.com>
2020-01-05 10:53:08 +0000

    Update wrapper.php

--- view/lec-admin/template/includes/filemanager/wrapper.php
+++ view/lec-admin/template/includes/filemanager/wrapper.php
@@ -11,9 +11,21 @@
 	<input type="hidden" name="fm_global_dir" id="fm_global_dir" value="<?php echo $directory; ?>" /> 
 
 	<?php include(DOC_ROOT.$this->_filemanager_top_bar_template); ?>
+	
+	<div class="units-row units-split end">
 
-	<div class="fm_content">
-	<?php $this->loadDirectoryContentsHTML($directory); ?>
+		<div class="unit-20 filetree">
+			
+			<?php //$this->generateFileTreeHTML($userid, $administrator); ?>
+		
+		</div>
+		
+		<div class="unit-80 fm_content">
+	
+			<?php $this->loadDirectoryContentsHTML($directory); ?>
+			
+		</div>
+	
 	</div>
 	
 	<div class="fm_footer">
