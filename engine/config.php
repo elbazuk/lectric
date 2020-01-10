0df28631a1554adb10f411401fbdce3889ccd61b
Elliott Barratt <elbazuk@gmail.com>
2020-01-10 09:51:02 +0000

    secure sessions

--- engine/config.php
+++ engine/config.php
@@ -139,6 +139,18 @@
 	*/
 		if (( !in_array(trim(URL_REQUEST,'/'), SESSION_IGNORES) )){
 			if (session_status() == PHP_SESSION_NONE) {
+				
+				// **PREVENTING SESSION HIJACKING**
+				// Prevents javascript XSS attacks aimed to steal the session ID
+				ini_set('session.cookie_httponly', 1);
+
+				// **PREVENTING SESSION FIXATION**
+				// Session ID cannot be passed through URLs
+				ini_set('session.use_only_cookies', 1);
+
+				// Uses a secure connection (HTTPS) if possible
+				ini_set('session.cookie_secure', 1);
+				
 				session_start();
 			}
 		}
